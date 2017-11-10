7cfa2ea6ce14fc093adce93ee641c6ccdad6fd25
WangGenshen <847315251@qq.com>
2017-11-10 16:44:10 +0800

    增加JasperReport项目代码

---
+++ JavaEE Framework/JasperReport2017/JasperReport/src/main/java/JasperReportCompile.java
@@ -0,0 +1,43 @@
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/11/8.
+ */
+public class JasperReportCompile {
+
+    public static void main(String[] args) {
+        try {
+            // 第二步：把jrxml文档编译成jasper文档
+            String jasperName = JasperCompileManager.compileReportToFile("src/main/resources/report1.jrxml"); // .jasper文件，文件名与jrxml的文件名一致
+            System.out.println(jasperName);
+            //第三步：获取填充报表的数据，　数据库
+            List<Sale> saleList = getSaleList();
+            // 第四步：组装报表数据源
+            JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(saleList);
+            // 第五步：填充报表
+            Map<String, Object> params = new HashMap<String, Object>();
+            String printName = JasperFillManager.fillReportToFile("src/main/resources/report1.jasper", params, beanColDataSource);
+            System.out.println(printName);
+            // 第六步：打印或导出报表
+            // JasperPrintManager.printReport(printName, true); // 使用打印机打印报表
+            JasperExportManager.exportReportToPdfFile(printName);
+        } catch (JRException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<Sale> getSaleList() {
+        List<Sale> saleList = new ArrayList<Sale>();
+        saleList.add(new Sale(1, 1, 100));
+        saleList.add(new Sale(2, 2, 120));
+        saleList.add(new Sale(3, 3, 150));
+        return saleList;
+    }
+
+}
---
+++ JavaEE Framework/JasperReport2017/JasperReport/src/main/java/Sale.java
@@ -0,0 +1,42 @@
+/**
+ * Created by Administrator on 2017/11/9.
+ */
+public class Sale {
+
+    private Integer id;
+    private Integer month;
+    private Integer quantity;
+
+    public Sale() {
+    }
+
+    public Sale(Integer id, Integer month, Integer quantity) {
+        this.id = id;
+        this.month = month;
+        this.quantity = quantity;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public void setMonth(Integer month) {
+        this.month = month;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
